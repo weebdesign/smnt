c6bb20b17b4f4d77f3f3d67983fa13d702f5f57d
weebdesign <155826765+weebdesign@users.noreply.github.com>
2024-03-08 11:16:33 +0600

    Add files via upload

---
+++ New folder4/Part-14 - responsive-web-page/resoucese/js/custom.js
@@ -0,0 +1,30 @@
+
+
+        $(document).ready (function(){
+
+// For  sticky position
+
+$('.button-parent').click(function(){
+    $('.role').hide();
+});
+
+
+
+    // miixitup for [portfolio section]
+    var mixer = mixitup(".container");
+
+    
+   
+        
+    });
+    
+    $(".portfolio-section").ripples({
+        resulation:512,
+        dropredius:20,
+        perturbance:0.07,
+    });
+    
+
+
+
+   
